4d5ba2ee665cd993915befcc039de1ef19e2160b
Lawrence Hubbell <nathan@pellego.com>
2017-07-25 12:41:31 -0700

    updated the package files and added new files for a simple server

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+// app.use(express.static('public'));
+
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address()
+   var port = server.address().port
+   console.log(`Example app listening at http://${host}:${port}`)
+   console.log('host ', host)
+})
