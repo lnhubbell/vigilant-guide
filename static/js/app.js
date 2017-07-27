822e3c008be5cee4bacbb920e65b8d1152863d64
Laura Illig <laura@pellego.com>
2017-07-26 19:31:05 -0700

    newFoods object for user to edit

--- static/js/app.js
+++ static/js/app.js
@@ -14,33 +14,21 @@ Vue.filter('round', function(value, decimals) {
 var nutritionApp = new Vue({
     el: '#nutritionApp',
     data: {
+        // search results
         foods: [],
+        // foods selected by user and added to the log
         selected_foods: [],
         newFood: {
             name: '',
             meal: '',
             servingType: '',
             servingSize: '',
+            foodObj: {},
         },
-        meals: [
-            {
-                name: 'breakfast',
-                foods: [],
-            },
-            {
-                name: 'lunch',
-                foods: [],
-            },
-            {
-                name: 'dinner',
-                foods: [],
-            },
-            {
-                name: 'snacks',
-                foods: [],
-            }
-        ],
+        // TODO [lji] why does removing this throw an error?
         newfood: undefined,
+        meals: [ 'breakfast', 'lunch', 'dinner', 'snacks' ],
+        servingTypes: [],
         nutrient_ids: [
             "435",
             "406",
@@ -70,13 +58,39 @@ var nutritionApp = new Vue({
         }
     },
     methods: {
+        addNewFood: function() {
+            // calculate the nutrients based on the total servings
+            this.tallyNutrientsForSelection(this.newFood.foodObj);
+
+            this.selected_foods.push(this.newFood.foodObj);
+
+            // TODO [lji] Add error handling / required inputs
+
+            this.totalNutrients();
+
+            newFood = {
+                name: '',
+                meal: '',
+                servingType: '',
+                servingSize: '',
+                foodObj: {},
+            };
+        },
         fetchFoodData: function(fetch) {
+            // retrieves the search results for the user
             this.$http.get('http://127.0.0.1:3000/search?search_term=' + fetch).then(response => {
                 nutritionApp.foods = response.body.list.item;
             }, response => {
             });
         },
         selectFood: function(item) {
+            this.newFood = {
+                name: '',
+                meal: '',
+                servingType: '',
+                servingSize: '',
+                foodObj: {},
+            };
 
             this.$http.get('http://127.0.0.1:3000/item?ndbno=' + item.ndbno).then(response => {
 
@@ -100,35 +114,45 @@ var nutritionApp = new Vue({
                         };
                     }
                 }
+
+                for (var k=0; k<nutrients[k].measures.length; k++) {
+                    this.servingTypes.push(nutrients[k].measures[k].label)
+                }
+
                 console.log('processed item: ', item);
 
-                this.selected_foods.push(item);
-                this.totalNutrients();
+                this.newFood.name = item.name;
+                // clear the search results
+                this.foods = [];
 
             }, response => {
             });
 
         },
+        tallyNutrientsForSelection: function(food) {
+            console.log(food);
+        },
         totalNutrients: function() {
-            this.nutrient_totals = {
-                "435": 0,
-                "406": 0,
-                "405": 0,
-                "404": 0,
-                "318": 0,
-                "415": 0,
-                "418": 0
-            };
-            for (var i = this.selected_foods.length - 1; i >= 0; i--) {
-                var food = this.selected_foods[i]
-                this.nutrient_totals[435] += parseFloat(food.nutrients[435].value);
-                this.nutrient_totals[406] += parseFloat(food.nutrients[406].value);
-                this.nutrient_totals[405] += parseFloat(food.nutrients[405].value);
-                this.nutrient_totals[404] += parseFloat(food.nutrients[404].value);
-                this.nutrient_totals[318] += parseFloat(food.nutrients[318].value);
-                this.nutrient_totals[415] += parseFloat(food.nutrients[415].value);
-                this.nutrient_totals[418] += parseFloat(food.nutrients[418].value);
+            for (var i=0; i<this.nutrient_ids.length; i++) {
+                this.nutrient_totals[this.nutrient_ids[i]] = 0;
             }
+
+            for (var k = this.selected_foods.length - 1; k >= 0; k--) {
+                var food = this.selected_foods[k];
+
+                for (var j=0; j<this.nutrient_ids.length; j++) {
+                    this.nutrient_totals[this.nutrient_ids[j]] += parseFloat(food.nutrients[this.nutrient_ids[j]].value);
+                }
+                // this.nutrient_totals[435] += parseFloat(food.nutrients[435].value);
+                // this.nutrient_totals[406] += parseFloat(food.nutrients[406].value);
+                // this.nutrient_totals[405] += parseFloat(food.nutrients[405].value);
+                // this.nutrient_totals[404] += parseFloat(food.nutrients[404].value);
+                // this.nutrient_totals[318] += parseFloat(food.nutrients[318].value);
+                // this.nutrient_totals[415] += parseFloat(food.nutrients[415].value);
+                // this.nutrient_totals[418] += parseFloat(food.nutrients[418].value);
+            }
+
+            console.log(this.nutrient_totals)
 
         }
     },
