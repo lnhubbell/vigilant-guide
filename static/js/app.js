15fe73459073bb8165bd759eeb05ce3607793965
Lawrence Hubbell <nathan@pellego.com>
2017-07-26 20:14:49 -0700

    got a basic mvp recommendation working

--- static/js/app.js
+++ static/js/app.js
@@ -67,7 +67,18 @@ var nutritionApp = new Vue({
             "318": 1000,
             "415": 2,
             "418": 6
-        }
+        },
+        low_nutrients: [],
+        recommended_foods_map: {
+            "435": ["16357", "16370"],
+            "406": ["16097", "05718"],
+            "405": ["17225", "12061"],
+            "404": ["11811", "12151"],
+            "318": ["11124", "11510", "11790"],
+            "415": ["15221", "09037"],
+            "418": ["15088", "15047"]
+        },
+        recommended_foods: []
     },
     methods: {
         fetchFoodData: function(fetch) {
@@ -109,6 +120,35 @@ var nutritionApp = new Vue({
             });
 
         },
+        processFood: function(item) {
+            this.$http.get('http://127.0.0.1:3000/item?ndbno=' + item.ndbno).then(response => {
+
+                var someData = response.body;
+                var nutrients = someData.report.food.nutrients;
+                item.nutrients = {
+                    "435": 0,
+                    "406": 0,
+                    "405": 0,
+                    "404": 0,
+                    "318": 0,
+                    "415": 0,
+                    "418": 0
+
+                };
+                for (var i = nutrients.length - 1; i >= 0; i--) {
+                    if (this.nutrient_ids.indexOf(nutrients[i].nutrient_id) > -1) {
+                        item.nutrients[nutrients[i].nutrient_id] = {
+                            name: nutrients[i].name,
+                            value: nutrients[i].value
+                        };
+                    }
+                }
+                console.log('processed item: ', item);
+                return item
+
+            }, response => {
+            });
+        },
         totalNutrients: function() {
             this.nutrient_totals = {
                 "435": 0,
@@ -130,7 +170,63 @@ var nutritionApp = new Vue({
                 this.nutrient_totals[418] += parseFloat(food.nutrients[418].value);
             }
 
-        }
+        },
+        lowNutrients: function() {
+            this.low_nutrients = [];
+            for (var i = this.nutrient_ids.length - 1; i >= 0; i--) {
+                var nid = this.nutrient_ids[i];
+                if ((this.nutrient_minimums[nid] - this.nutrient_totals[nid]) > 0) {
+                    this.low_nutrients.push(nid);
+                };
+            }
+        },
+        getRecommendations: function() {
+            console.log('getting recommended_foods');
+            this.lowNutrients();
+            console.log('low_nutrients: ', this.low_nutrients);
+            for (var i = this.low_nutrients.length - 1; i >= 0; i--) {
+                var foods = this.recommended_foods_map[this.low_nutrients[i]];
+                console.log('foods', foods);
+                for (var f = foods.length - 1; f >= 0; f--) {
+                    var food = foods[f];
+                    this.recommendFood(food);
+                }
+                console.log('length: ', this.low_nutrients.length );
+            }
+        },
+        recommendFood: function(ndbno) {
+
+            this.$http.get('http://127.0.0.1:3000/item?ndbno=' + ndbno).then(response => {
+                var someData = response.body;
+                console.log('recommended food: ', someData);
+                var nutrients = someData.report.food.nutrients;
+                var item = someData.report.food;
+                item.nutrients = {
+                    "435": 0,
+                    "406": 0,
+                    "405": 0,
+                    "404": 0,
+                    "318": 0,
+                    "415": 0,
+                    "418": 0
+
+                };
+                for (var i = nutrients.length - 1; i >= 0; i--) {
+                    if (this.nutrient_ids.indexOf(nutrients[i].nutrient_id) > -1) {
+                        item.nutrients[nutrients[i].nutrient_id] = {
+                            name: nutrients[i].name,
+                            value: nutrients[i].value
+                        };
+                    }
+                }
+                console.log('processed item: ', item);
+
+                this.recommended_foods.push(item);
+
+            }, response => {
+            });
+
+        },
     },
 });
 
