c3047ba82af9beb8af2f4b1ceeb00e1c914f7a62
Laura Illig <laura@pellego.com>
2017-07-26 17:44:02 -0700

    selectors for meals and servings

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,12 @@ var nutritionApp = new Vue({
     data: {
         foods: [],
         selected_foods: [],
+        newFood: {
+            name: '',
+            meal: '',
+            servingType: '',
+            servingSize: '',
+        },
         meals: [
             {
                 name: 'breakfast',
@@ -21,13 +27,6 @@ var nutritionApp = new Vue({
                 foods: [],
             }
         ],
-        myfoods: [
-            // example data array
-            // {
-            //     name: undefined,
-            //     meal: undefined,
-            // },
-        ],
         newfood: undefined,
         nutrient_ids: [
             "435",
@@ -41,11 +40,6 @@ var nutritionApp = new Vue({
         nutrient_totals: {}
     },
     methods: {
-        addNewFood: function () {
-            console.log(this.meal.value)
-            console.log(this.meals)
-            console.log(this.newfood)
-        },
         fetchFoodData: function(fetch) {
             this.$http.get('http://127.0.0.1:3000/search?search_term=' + fetch).then(response => {
                 nutritionApp.foods = response.body.list.item;
@@ -67,15 +61,15 @@ var nutritionApp = new Vue({
                     "415": 0,
                     "418": 0
 
-                }
+                };
                 for (var i = nutrients.length - 1; i >= 0; i--) {
                     if (this.nutrient_ids.indexOf(nutrients[i].nutrient_id) > -1) {
                         item.nutrients[nutrients[i].nutrient_id] = {
                             name: nutrients[i].name,
                             value: nutrients[i].value
                         };
-                    };
-                };
+                    }
+                }
                 console.log('processed item: ', item);
 
                 this.selected_foods.push(item);
@@ -104,7 +98,7 @@ var nutritionApp = new Vue({
                 this.nutrient_totals[318] += parseFloat(food.nutrients[318].value);
                 this.nutrient_totals[415] += parseFloat(food.nutrients[415].value);
                 this.nutrient_totals[418] += parseFloat(food.nutrients[418].value);
-            };
+            }
 
         }
     },
